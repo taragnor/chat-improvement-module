c676c3fce38e806dda9f0b1b08e5e51a240926f7
Joe Martino <taragnor@yahoo.com>
2021-11-10 16:54:21 -0500

    added ghost text and ghost command

--- chattemplate.js
+++ chattemplate.js
@@ -5,7 +5,7 @@ Handlebars.registerHelper('if_str_includes', function(a, b, opts) {
 		return opts.fn(this);
 	} else {
 		if (!a.includes)
-			console.log(`includes not in $a`)
+			console.error(`includes not in ${a}`)
 		return opts.inverse(this);
 	}
 });
@@ -17,3 +17,58 @@ Handlebars.registerHelper('is_quote', function(a) {
 });
 
 CONFIG.ChatMessage.template = "modules/Improved-Chat-Template/template/chat-message.html"
+
+Hooks.on("chatMessage", (x, message, chatData)=> {
+	console.log("Calling hook");
+	if (message.startsWith("/ghost")) {
+		const cls = ChatMessage.implementation;
+		const ghostmsg = message.substring(6);
+		message =`<div class="ghost-text" data-alttext="die">${ghostmsg}</div>`;
+
+		message = message.replace(/\n/g, "<br>");
+		chatData.content = message;
+		console.log("Cancelling");
+		const createOptions = {};
+		cls.create(chatData, createOptions);
+		return false;
+	} else {
+		console.log(msg.content);
+	}
+});
+
+function ghostswap() {
+	$(".ghost-text").each( async function () {
+		const origtext=  this.textContent
+		this.textContent = ghostswap.generateGhostAltText(origtext);
+		await ghostswap.stall(200);
+		this.textContent = origtext;
+	});
+	setTimeout(ghostswap, 5000 + Math.random() * 30000);
+}
+
+ghostswap.generateGhostAltText = function (origtext) {
+	const subtextlist = ["DEATHdieDEATH", "die", "THEEND", "FINALDESTINATION", "X", "VERYSOON"]
+		.filter( x=> x.length <= origtext.length) ?? "";
+	const subtext = subtextlist[Math.floor(Math.random() * subtextlist.length)];
+	const subtextlen = subtext.length;
+	let pos = 0;
+	let ret ="";
+	for (const letter of origtext) {
+		if (/\s/.test(letter))
+			ret += letter;
+		else
+			ret += subtext[ pos++ % subtextlen ];
+	}
+	return ret;
+}
+
+ghostswap.stall = function (time) {
+			return new Promise( (keep, brk) => {
+				setTimeout( keep, time);
+			});
+}
+
+ghostswap();
+
+
+
